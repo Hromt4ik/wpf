f534951e51c9d82b0f1a8fbbd46c51a4ea3a2a9e
Hromt4ik <97237642+Hromt4ik@users.noreply.github.com>
2023-03-24 22:20:15 +0500

    Update AddWindow.xaml.cs

--- lr7/AddWindow.xaml.cs
+++ lr7/AddWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,46 @@ namespace Lr4
         public AddWindow()
         {
             InitializeComponent();
+            companyView.ItemsSource = DatabaseControl.GetCompanyForView();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            Phone _tempPhone = new Phone();
+
+            try
+            {
+                if (Check.CheckEmpty(companyView.SelectedValue, Convert.ToDecimal(priceView.Text), titleView.Text))
+                {
+                    _tempPhone.Title = titleView.Text;
+                    _tempPhone.CompanyId = (int)companyView.SelectedValue;
+                    _tempPhone.Price = Convert.ToDecimal(priceView.Text);
+                }
+
+                if (Check.check(_tempPhone))
+                {
+                    DatabaseControl.AddPhone(new Phone
+                    {
+                        Title = titleView.Text,
+                        CompanyId = (int)companyView.SelectedValue,
+                        Price = Convert.ToDecimal(priceView.Text)
+                    });
+
+                    (this.Owner as MainWindow).RefreshTable();
+                    this.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Неверный формат дынных", "Ошибка при вводе", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
